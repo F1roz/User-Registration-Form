83bc366acc8b6abc7f74c861163f657c22045f21
Firoz fahim <52341506+F1roz@users.noreply.github.com>
2021-11-10 14:13:25 +0600

    Add files via upload

---
+++ UserReg/UserRegistration.Designer.cs
@@ -0,0 +1,311 @@
+﻿
+namespace UserReg
+{
+    partial class UserRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.otherradioButton = new System.Windows.Forms.RadioButton();
+            this.femaleradioButton = new System.Windows.Forms.RadioButton();
+            this.maleradioButton = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dobdateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.confirmpasswordtextbox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.passwordtextbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.emailtextbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.usernametextbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nametextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BGcomboBox = new System.Windows.Forms.ComboBox();
+            this.TermscheckBox = new System.Windows.Forms.CheckBox();
+            this.Submitbutton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Submitbutton);
+            this.groupBox1.Controls.Add(this.TermscheckBox);
+            this.groupBox1.Controls.Add(this.BGcomboBox);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.otherradioButton);
+            this.groupBox1.Controls.Add(this.femaleradioButton);
+            this.groupBox1.Controls.Add(this.maleradioButton);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.dobdateTimePicker1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.confirmpasswordtextbox);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.passwordtextbox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.emailtextbox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.usernametextbox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.nametextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(23, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(639, 570);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Registration Form";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(43, 431);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Blood Group";
+            // 
+            // otherradioButton
+            // 
+            this.otherradioButton.AutoSize = true;
+            this.otherradioButton.Location = new System.Drawing.Point(401, 372);
+            this.otherradioButton.Name = "otherradioButton";
+            this.otherradioButton.Size = new System.Drawing.Size(78, 24);
+            this.otherradioButton.TabIndex = 15;
+            this.otherradioButton.TabStop = true;
+            this.otherradioButton.Text = "Other";
+            this.otherradioButton.UseVisualStyleBackColor = true;
+            // 
+            // femaleradioButton
+            // 
+            this.femaleradioButton.AutoSize = true;
+            this.femaleradioButton.Location = new System.Drawing.Point(287, 372);
+            this.femaleradioButton.Name = "femaleradioButton";
+            this.femaleradioButton.Size = new System.Drawing.Size(97, 24);
+            this.femaleradioButton.TabIndex = 14;
+            this.femaleradioButton.TabStop = true;
+            this.femaleradioButton.Text = "Female ";
+            this.femaleradioButton.UseVisualStyleBackColor = true;
+            // 
+            // maleradioButton
+            // 
+            this.maleradioButton.AutoSize = true;
+            this.maleradioButton.Location = new System.Drawing.Point(183, 372);
+            this.maleradioButton.Name = "maleradioButton";
+            this.maleradioButton.Size = new System.Drawing.Size(72, 24);
+            this.maleradioButton.TabIndex = 13;
+            this.maleradioButton.TabStop = true;
+            this.maleradioButton.Text = "Male";
+            this.maleradioButton.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(95, 372);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Gender";
+            // 
+            // dobdateTimePicker1
+            // 
+            this.dobdateTimePicker1.Location = new System.Drawing.Point(183, 306);
+            this.dobdateTimePicker1.Name = "dobdateTimePicker1";
+            this.dobdateTimePicker1.Size = new System.Drawing.Size(323, 28);
+            this.dobdateTimePicker1.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 313);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Date Of Birth";
+            // 
+            // confirmpasswordtextbox
+            // 
+            this.confirmpasswordtextbox.Location = new System.Drawing.Point(183, 250);
+            this.confirmpasswordtextbox.Name = "confirmpasswordtextbox";
+            this.confirmpasswordtextbox.PasswordChar = '*';
+            this.confirmpasswordtextbox.Size = new System.Drawing.Size(323, 28);
+            this.confirmpasswordtextbox.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(-2, 258);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Confirm Password";
+            // 
+            // passwordtextbox
+            // 
+            this.passwordtextbox.Location = new System.Drawing.Point(183, 200);
+            this.passwordtextbox.Name = "passwordtextbox";
+            this.passwordtextbox.PasswordChar = '*';
+            this.passwordtextbox.Size = new System.Drawing.Size(323, 28);
+            this.passwordtextbox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password";
+            // 
+            // emailtextbox
+            // 
+            this.emailtextbox.Location = new System.Drawing.Point(183, 150);
+            this.emailtextbox.Name = "emailtextbox";
+            this.emailtextbox.Size = new System.Drawing.Size(323, 28);
+            this.emailtextbox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Email";
+            // 
+            // usernametextbox
+            // 
+            this.usernametextbox.Location = new System.Drawing.Point(182, 109);
+            this.usernametextbox.Name = "usernametextbox";
+            this.usernametextbox.Size = new System.Drawing.Size(324, 28);
+            this.usernametextbox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "UserName";
+            // 
+            // nametextBox
+            // 
+            this.nametextBox.Location = new System.Drawing.Point(182, 50);
+            this.nametextBox.Name = "nametextBox";
+            this.nametextBox.Size = new System.Drawing.Size(324, 28);
+            this.nametextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // BGcomboBox
+            // 
+            this.BGcomboBox.FormattingEnabled = true;
+            this.BGcomboBox.Items.AddRange(new object[] {
+            "A+",
+            "A-",
+            "B+",
+            "B-",
+            "AB+",
+            "O+",
+            "O-"});
+            this.BGcomboBox.Location = new System.Drawing.Point(183, 428);
+            this.BGcomboBox.Name = "BGcomboBox";
+            this.BGcomboBox.Size = new System.Drawing.Size(121, 27);
+            this.BGcomboBox.TabIndex = 17;
+            // 
+            // TermscheckBox
+            // 
+            this.TermscheckBox.AutoSize = true;
+            this.TermscheckBox.Location = new System.Drawing.Point(146, 480);
+            this.TermscheckBox.Name = "TermscheckBox";
+            this.TermscheckBox.Size = new System.Drawing.Size(333, 24);
+            this.TermscheckBox.TabIndex = 18;
+            this.TermscheckBox.Text = "I agree all the terms and conditions";
+            this.TermscheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Submitbutton
+            // 
+            this.Submitbutton.Location = new System.Drawing.Point(265, 523);
+            this.Submitbutton.Name = "Submitbutton";
+            this.Submitbutton.Size = new System.Drawing.Size(97, 29);
+            this.Submitbutton.TabIndex = 19;
+            this.Submitbutton.Text = "Submit";
+            this.Submitbutton.UseVisualStyleBackColor = true;
+            // 
+            // UserRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 694);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "UserRegistration";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "User Registration";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UserRegistration_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox confirmpasswordtextbox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox passwordtextbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox emailtextbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox usernametextbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nametextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dobdateTimePicker1;
+        private System.Windows.Forms.RadioButton maleradioButton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton otherradioButton;
+        private System.Windows.Forms.RadioButton femaleradioButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox BGcomboBox;
+        private System.Windows.Forms.Button Submitbutton;
+        private System.Windows.Forms.CheckBox TermscheckBox;
+    }
+}
+
---
+++ UserReg/UserRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserReg
+{
+    public partial class UserRegistration : Form
+    {
+        public UserRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void UserRegistration_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+       
+    }
+}
